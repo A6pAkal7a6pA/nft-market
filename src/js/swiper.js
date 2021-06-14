4258c8229d9ca0d63f4e473ef2dce272b4cecd35
Maksym Kuleshov <maksym_kuleshov@epam.com>
2021-06-14 22:36:42 +0300

    init

---
+++ src/js/swiper.js
@@ -0,0 +1,48 @@
+import Swiper from "swiper/bundle";
+
+new Swiper(".main-slider", {
+	loop: false,
+	speed: 1000,
+	effect: 'coverflow',
+	slidesPerView: "auto",
+	initialSlide: 0,
+	centeredSlides: false,
+	coverflowEffect: {
+		rotate: 0,
+		stretch: 200,
+		depth: 100,
+		modifier: 2,
+		slideShadows: false,
+	},	
+	pagination: {
+		el: '.main-slider__dots',
+		clickable: true,
+	},
+	breakpoints: {
+		320: {
+			effect: "slide",
+			slidesPerView: 1.5,
+			spaceBetween: 10,
+		},
+		500: {
+			loop: false,
+			speed: 1000,
+			effect: 'coverflow',
+			slidesPerView: "auto",
+			initialSlide: 0,
+			centeredSlides: false,
+			coverflowEffect: {
+				rotate: 0,
+				stretch: 200,
+				depth: 100,
+				modifier: 2,
+				slideShadows: false,
+			},	
+			pagination: {
+				el: '.main-slider__dots',
+				clickable: true,
+			},
+		},
+	}
+
+})
